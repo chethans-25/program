e4496ee91c6efd2e4034469564f3db17f1d21223
Chethan <schethanchandu5gmail.com>
2023-08-22 21:27:26 +0530

    aug 22

--- CNP/trial.c
+++ CNP/trial.c
@@ -1,25 +1,78 @@
-#include <stdio.h>
-#include <math.h>
-
-int rem(int x, int y){
-    int REM=x-y*(x/y);
-    return REM;
-}
-
-int powerr(int a,int b){
-int p,x=b;
-if (x<=0) return 1;
-else if(x==1) return a;
-else return a*powerr(a,x-1);
-}
+/*
+//Encryption by transposition method
+#include<stdio.h>
+#include<string.h>
 
 void main(){
-    char i[26]="^A^B";
-    for (j=0;j<s)
+char data[100];
+char wrd[]="MEGABUCK";
+char cipher[20][8];
+int seq[8];
+int i,j,k,cnt;
+for (i=0;i<len;i++){
+cnt=0;
+for(j=0;j<8;j++)
+if(wrd[i]>wrd[j]) ++cnt;
+seq[i]=cnt;
+}//end of for i
+printf("\n Enter data :");
+gets(data);
+cnt=strlen(data);
+for(i=0;i<cnt;i++) cipher[i/len][i%len]=data[i];
+if(i%len!=0){
+for(j=i%len;j<len;j++){
+cipher[i/len][j]='.';
+cnt++;
+}//end of for j
+}//end of if
+printf("\n Encrypted data :\n");
+for(i=0;i<8;i++){
+for(j=0;j<8;j++)
+if(seq[j]==i) break;
+for(k=0;k<cnt/8||k==0;k++) printf("%c",cipher[k][j]);
+}//end of for i
+printf("\n");
+}//end of main
+*/
 
-//     for( int j=0;j<26;j++){
-//         printf("%d\n",scanf("%c",&i));
-//     }
-// 
+//modified code
 
-}
+//Encryption by transposition method
+#include<stdio.h>
+#include<string.h>
+
+
+void main(){
+char data[100];
+char wrd[]="MEGABUCK";
+int len=strlen(wrd);
+char cipher[20][len];
+int seq[len];
+int i,j,k,cnt;
+
+for (i=0;i<len;i++){
+    cnt=0;
+    for(j=0;j<len;j++)
+        if(wrd[i]>wrd[j]) {cnt++;}
+    seq[i]=cnt;
+}//end of for i
+
+printf("\n Enter data :");
+cnt=strlen(gets(data));
+for(i=0;i<cnt;i++) cipher[i/len][i%len]=data[i];
+if(i%len!=0){
+    for(j=i%len;j<len;j++){
+        cipher[i/len][j]='.';
+        cnt++;
+    }//end of for j
+}//end of if
+printf("\n Encrypted data :\n");
+for(i=0;i<len;i++){
+    for(j=0;j<len;j++)
+        if(seq[j]==i) break;
+    for(k=0;k<cnt/len||k==0;k++)
+        printf("%c",cipher[k][j]);
+}//end of for i
+printf("\n");
+
+}//end of main
