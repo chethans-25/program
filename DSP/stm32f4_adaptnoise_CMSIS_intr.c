88c5478d8bf89f21c41c418eb5b0822bc8e7a6b5
Chethan <schethanchandu5gmail.com>
2023-09-12 14:14:29 +0530

    dsp

--- DSP/stm32f4_adaptnoise_CMSIS_intr.c
+++ DSP/stm32f4_adaptnoise_CMSIS_intr.c
@@ -2,19 +2,19 @@
 
 #include "stm32f4_wm5102_init.h"
 
-#define SAMPLING_FREQ 8000
-#define NOISE_FREQ 1200.0
-#define SIGNAL_FREQ 2500.0
-#define NOISE_AMPLITUDE 8000.0
-#define SIGNAL_AMPLITUDE 8000.0
+#define Fs 8000
+#define Fn 1200.0
+#define Fsig 2500.0
+#define An 8000.0
+#define As 8000.0
 
 #define BETA 1E-12         // adaptive learning rate      
 #define NUM_TAPS 10        // number of weights
 #define BLOCK_SIZE 1
 
-float32_t theta_increment_noise;
+float32_t inc_noise;
 float32_t theta_noise = 0.0;
-float32_t theta_increment_signal;
+float32_t inc_sig;
 float32_t theta_signal = 0.0;
 float32_t yout, error, signal, signoise, sigplusnoise, refnoise;
 
@@ -36,16 +36,16 @@ void SPI2_IRQHandler()
   	  left_in_sample = SPI_I2S_ReceiveData(I2Sx); // input sample not used
 
       GPIO_SetBits(GPIOD, GPIO_Pin_15);           // algorithm timing
-      theta_increment_noise = 2*PI*NOISE_FREQ/SAMPLING_FREQ;
-      theta_noise += theta_increment_noise;
+      inc_noise = 2*PI*Fn/Fs;
+      theta_noise += inc_noise;
       if (theta_noise > 2*PI) theta_noise -= 2*PI;
-      theta_increment_signal = 2*PI*SIGNAL_FREQ/SAMPLING_FREQ;
-      theta_signal += theta_increment_signal;
+      inc_sig = 2*PI*Fsig/Fs;
+      theta_signal += inc_sig;
       if (theta_signal > 2*PI) theta_signal -= 2*PI;
 
-      refnoise = (NOISE_AMPLITUDE*arm_cos_f32(theta_noise));  
-      signoise = (NOISE_AMPLITUDE*arm_sin_f32(theta_noise));
-      signal = (SIGNAL_AMPLITUDE*arm_sin_f32(theta_signal));
+      refnoise = (An*arm_cos_f32(theta_noise));  
+      signoise = (An*arm_sin_f32(theta_noise));
+      signal = (As*arm_sin_f32(theta_signal));
       sigplusnoise = signoise+signal;
 	
       arm_lms_f32(&S, &refnoise, &sigplusnoise, &yout, &error, 1);
